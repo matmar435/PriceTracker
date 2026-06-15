82f0d48ffe5e0b8d6fe7cc2d5f806b43c04f0fb8
matmar435 <matmar435@gmail.com>
2026-06-15 14:32:21 +0200

    feat: configure database context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product_Price_Tracker.Entities;
+
+namespace Product_Price_Tracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
--- Entities/Product.cs
+++ Entities/Product.cs
@@ -2,5 +2,16 @@
 {
     public class Product
     {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Url { get; set; } = string.Empty;
+
+        public decimal TargetPrice { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
