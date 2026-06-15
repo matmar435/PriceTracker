2d83a9be0b87f997ae4af421d00d166858baba72
matmar435 <matmar435@gmail.com>
2026-06-15 15:36:45 +0200

    fix: rename csproj

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Product_Price_Tracker.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 
