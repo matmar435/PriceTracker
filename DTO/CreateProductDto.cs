acfe7b659aaf2ccd710bcf670fe5d0a1403fafbc
matmar435 <matmar435@gmail.com>
2026-06-17 21:38:21 +0200

    feat: add product validation

--- DTO/CreateProductDto.cs
+++ DTO/CreateProductDto.cs
@@ -1,11 +1,18 @@
-﻿namespace Product_Price_Tracker.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_Price_Tracker.DTO
 {
     public class CreateProductDto
     {
+        [Required]
+        [StringLength(200)]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
+        [Url]
         public string Url { get; set; } = string.Empty;
 
+        [Range(0.01, double.MaxValue)]
         public decimal TargetPrice { get; set; }
     }
 }
