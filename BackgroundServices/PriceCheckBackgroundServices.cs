b0ad532a7163c9feaddf1938af035105a191bdef
matmar435 <matmar435@gmail.com>
2026-06-19 21:23:28 +0200

    feat: add background price monitoring services

---
+++ BackgroundServices/PriceCheckBackgroundServices.cs
@@ -0,0 +1,57 @@
+﻿using Product_Price_Tracker.Entities;
+using Product_Price_Tracker.Provider.Interfaces;
+
+namespace Product_Price_Tracker.BackgroundServices;
+
+public class PriceCheckBackgroundService : BackgroundService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public PriceCheckBackgroundService(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    protected override async Task ExecuteAsync(
+        CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var repository =
+                scope.ServiceProvider.GetRequiredService<IProductRepository>();
+
+            var priceProvider =
+                scope.ServiceProvider.GetRequiredService<IPriceProvider>();
+
+            var products = await repository.GetAllAsync();
+
+            foreach (var product in products)
+            {
+                var newPrice =
+                    await priceProvider.GetCurrentPriceAsync(product.Url);
+
+                if (newPrice is null)
+                    continue;
+
+                product.CurrentPrice = newPrice.Value;
+
+                await repository.AddPriceHistoryAsync(
+                    new PriceHistory
+                    {
+                        Id = Guid.NewGuid(),
+                        ProductId = product.Id,
+                        Price = newPrice.Value,
+                        CheckedAt = DateTime.UtcNow
+                    });
+            }
+
+            await repository.SaveChangesAsync();
+
+            await Task.Delay(
+                TimeSpan.FromMinutes(1),
+                stoppingToken);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Product_Price_Tracker.BackgroundServices;
 using Product_Price_Tracker.Data;
 using Product_Price_Tracker.Provider;
 using Product_Price_Tracker.Provider.Interfaces;
 using Product_Price_Tracker.Services;
 using Product_Price_Tracker.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,6 +25,8 @@ builder.Services.AddScoped<IPriceProvider, MockPriceProvider>();
 builder.Services.AddScoped<IPriceScraperService, PriceScraperService>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ProductService>();
+
+builder.Services.AddHostedService<PriceCheckBackgroundService>();
 
 var app = builder.Build();
 
